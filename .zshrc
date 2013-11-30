0c92edecb7fda89283bbb6ac0098ea774e08ba6d
Chris Borgia <chris@cborgia.me>
2013-11-30 15:09:56 -0500

    how i used to rock my dots, changing now..

--- .zshrc
+++ .zshrc
@@ -4,16 +4,12 @@ ZSH_THEME="borgia"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git brew gem bower forklift)
+plugins=(bower brew gem git grunt forklift composer rvm)
 
 source $ZSH/oh-my-zsh.sh
 
-# Customize to your needs...
-export PATH=$PATH:~/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
-
-# Source your .alias_ files
-for file in ~/.alias_*; do
-    source "$file"
+# Load some helpufl dotfiles...
+for file in ~/.{private,bash_prompt,exports,alias*,functions,path}; do
+	[ -r "$file" ] && source "$file"
 done
-
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+unset file
---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")"
+
+# git pull origin master
+
+# Ask for the administrator password
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+function doIt() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+	source ~/.bash_profile
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
+
+# symlink Themes into their required folders
+ln -s ~/Content/themes/borgia.zsh-theme ~/.oh-my-zsh/themes
+
+
