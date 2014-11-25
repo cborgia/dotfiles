e5b65fc601c131507593b043596672b75f8825ca
Chris Borgia <chris@cborgia.me>
2014-11-25 12:51:29 -0500

    latest changes

--- .zshrc
+++ .zshrc
@@ -3,7 +3,7 @@ ZSH_THEME="borgia"
 
 source $ZSH/oh-my-zsh.sh
 
-# Load some helpufl dotfiles...
+# Load some dotfiles
 for file in ~/.{private,bash_prompt,exports,aliases*,functions,path}; do
 	[ -r "$file" ] && source "$file"
 done
@@ -11,5 +11,5 @@ unset file
 
 plugins=(git knife brew node npm osx vagrant composer)
 
-# # Customize to your needs...
-# export PATH=~/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin:/Applications/adt-bundle-mac-x86_64-20131030/sdk/platform-tools:$PATH
+# Customize to your needs...
+export PATH=~/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin:~/.composer/vendor/bin:$PATH
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE}")"
+
 git pull origin master
-
-# Ask for the administrator password
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -26,8 +19,3 @@ else
 	fi
 fi
 unset doIt
-
-# symlink theme into their required folder
-ln -s ~/Content/themes/borgia.zsh-theme ~/.oh-my-zsh/themes
-
-
