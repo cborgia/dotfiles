7755ec9ac29212f4290e1377c315890c46124131
chris <me@cborgia.me>
2017-10-18 14:10:53 -0400

    php71

--- mods/php/install.sh
+++ mods/php/install.sh
@@ -1,6 +1,13 @@
+# install php via homebrew
+brew tap homebrew/homebrew-php
+brew install php71
+
+
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/installer laravel/lumen-installer tightenco/jigsaw spatie/http-status-check bramus/mixed-content-scan laravel/spark-installer
+
+composer global update
