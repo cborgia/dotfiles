761b4b31160777696bc6f05ae5f2324187d3714f
Chris Borgia <me@cborgia.me>
2019-02-24 11:48:49 -0500

    latest php is fine
    no need to tag a release at this time

--- mods/php/install.sh
+++ mods/php/install.sh
@@ -1,9 +1,9 @@
 # install php via homebrew
-brew install php@7.3
+brew install php
 
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+# Install Composer (installed via brew now)
+# curl -sS https://getcomposer.org/installer | php
+# mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/valet laravel/installer laravel/lumen-installer tightenco/jigsaw spatie/http-status-check bramus/mixed-content-scan laravel/spark-installer
