93d973deff6f56c90489ede44a0a184325905ff5
Chris <chris@cborgia.me>
2021-02-05 11:10:03 -0500

    private alias for projects
    Signed-off-by: Chris <chris@cborgia.me>

--- mods/shell/aliases_private.zsh
+++ mods/shell/aliases_private.zsh
@@ -3,3 +3,5 @@ alias repos='cd ~/repos'
 
 alias cs='cd ~/repos/mine/ClientSalt'
 alias tt='cd ~/repos/mine/Techtegrity'
+alias mms='cd ~/repos/mine/MaintainMySite'
+alias vr='cd ~/repos/mine/Valrank'
